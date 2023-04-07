8758ac8c90c8be3970c68aaf613f7fc06f338187
ebr <clkebr2601@gmail.com>
2023-04-07 12:14:43 +0200

    Address service and necessary files are created

---
+++ src/main/java/com/account/service/implementation/AddressServiceImpl.java
@@ -0,0 +1,8 @@
+package com.account.service.implementation;
+
+import com.account.service.AddressService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+}
