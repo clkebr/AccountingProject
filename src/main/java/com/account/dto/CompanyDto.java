248e7796d846a1e69759424f6a2f4c61a56aa4cc
ebr <clkebr2601@gmail.com>
2023-04-07 12:15:31 +0200

    Company service and necessary files are created

--- src/main/java/com/account/dto/CompanyDto.java
+++ src/main/java/com/account/dto/CompanyDto.java
@@ -1,8 +1,16 @@
 package com.account.dto;
 
+import com.account.enums.CompanyStatus;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor
 public class CompanyDto {
+
+    private Long id;
+    private String title;
+    private String phone;
+    private String website;
+    private AddressDto address;
+    private CompanyStatus companyStatus;
 }
---
+++ src/main/java/com/account/service/implementation/CompanyServiceImpl.java
@@ -0,0 +1,8 @@
+package com.account.service.implementation;
+
+import com.account.service.CompanyService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+}
